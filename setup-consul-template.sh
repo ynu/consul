6a6d423e83b5e8e8bb3c5672df3393cbe3546375
na57 <na57@qq.com>
2015-11-26 14:52:27 +0800

    fix：config.hl脚本写入出错

--- setup-consul-template.sh
+++ setup-consul-template.sh
@@ -15,17 +15,18 @@ rm -f consul-template_0.11.1_linux_amd64.zip
 
 
 # 设置consul template的supervisor配置文件
+cd /etc/supervisor/conf.d/
 echo [program:consul-template] >> consul-template.conf
-echo command=consul-template -config /data/consul-template/config.hcl >> consul.conf
+echo command=consul-template -config /data/consul-template/config.hcl >> consul-template.conf
 
 # 设置默认的consul template模板及配置文件
 cd /data/consul-template
 touch example.ct
-echo consul = "10.0.2.15:8500" >> config.hcl
-echo token="" >> config.hcl
-echo template { >> config.hcl
-echo 	source = "/data/consul-template/example.ct" >> config.hcl
-echo 	destination = "/data/consul-template/example.conf" >> config.hcl
+echo 'consul = "10.0.2.15:8500"' >> config.hcl
+echo 'token=""' >> config.hcl
+echo 'template' { >> config.hcl
+echo '	source = "/data/consul-template/example.ct"' >> config.hcl
+echo '	destination = "/data/consul-template/example.conf"' >> config.hcl
 echo } >> config.hcl
 
 # 重新加载supervisor的配置
