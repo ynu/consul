b08f34fa9febccd14db281b933c1977e172a9dbc
na57 <na57@qq.com>
2015-12-02 14:01:25 +0800

    add: script for setting up etcd

--- setup-etcd.sh
+++ setup-etcd.sh
@@ -11,9 +11,13 @@ cp etcd-v2.2.2-linux-amd64/etcdctl /usr/bin
 rm -f etcd-v2.2.2-linux-amd64.tar.gz
 rm -rf etcd-v2.2.2-linux-amd64
 
+## 设置数据目录
+mkdir /data/etcd
+mkdir -p /data/etcd/data /data/etcd/keys
+
 # 设置etcd的supervisor配置文件
 cd /etc/supervisor/conf.d/
 echo '[program:etcd]' >> etcd.conf
-echo 'command=/usr/bin/etcd' >> etcd.conf
+echo 'command=/usr/bin/etcd -name etcd-0 -data-dir /data/etcd/data -advertise-client-urls=http://localhost:2379' >> etcd.conf
 
 supervisorctl reload
