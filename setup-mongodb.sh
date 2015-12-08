d5bf831d0eb4fc5eca96b8bcf84986280fb3affc
na57 <na57@qq.com>
2015-12-08 19:38:24 +0800

    add: setup mongodb

---
+++ setup-mongodb.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash
+
+# 安装 MongoDB
+sudo su
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+apt-get update
+apt-get install -y mongodb-org
